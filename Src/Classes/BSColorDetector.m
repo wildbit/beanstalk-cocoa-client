b896af9d71a373dd589758f38c46c6ad00f6bd26
Chris Ledet <christopher.ledet@gmail.com>
2013-06-30 00:38:50 -0400

    Update Colors

--- Src/Classes/BSColorDetector.m
+++ Src/Classes/BSColorDetector.m
@@ -47,23 +47,23 @@
 {
     return @{
              @"label-pink":         [UIColor colorWithHexString:@"FDA7CC"],
-             @"label-red":          [UIColor redColor],
+             @"label-red":          [UIColor colorWithHexString:@"ff5454"],
              @"label-red-orange":   [UIColor colorWithHexString:@"ff7733"],
-             @"label-orange":       [UIColor orangeColor],
-             @"label-yellow":       [UIColor yellowColor],
+             @"label-orange":       [UIColor colorWithHexString:@"FFA93A"],
+             @"label-yellow":       [UIColor colorWithHexString:@"F7D737"],
              @"label-yellow-green": [UIColor colorWithHexString:@"57D200"],
              @"label-aqua-green":   [UIColor colorWithHexString:@"73E5AC"],
-             @"label-green":        [UIColor greenColor],
+             @"label-green":        [UIColor colorWithHexString:@"8abf86"],
              @"label-green-blue":   [UIColor colorWithHexString:@"7fbfbf"],
              @"label-sky-blue":     [UIColor colorWithHexString:@"66e6ff"],
              @"label-light-blue":   [UIColor colorWithHexString:@"99d5ff"],
-             @"label-blue":         [UIColor blueColor],
+             @"label-blue":         [UIColor colorWithHexString:@"85aaf2"],
              @"label-orchid":       [UIColor colorWithHexString:@"e493fd"],
              @"label-violet":       [UIColor colorWithHexString:@"bf87be"],
-             @"label-brown":        [UIColor brownColor],
-             @"label-black":        [UIColor blackColor],
-             @"label-grey":         [UIColor grayColor],
-             };
+             @"label-brown":        [UIColor colorWithHexString:@"bf9985"],
+             @"label-black":        [UIColor colorWithHexString:@"222222"],
+             @"label-grey":         [UIColor colorWithHexString:@"AAAAAA"],
+            };
 }
 
 @end
